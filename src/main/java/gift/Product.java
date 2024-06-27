b9fb5e6ffdf0a4341789b7a21cf8fbfa009c44d9
eunsoni <esk12704@gmail.com>
2024-06-28 04:43:24 +0900

    add Product add function

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -5,7 +5,7 @@ public class Product {
     private String name;
     private int price;
     private String imageUrl;
-
+    public Product() {}
     public Product(long id, String name, int price, String imageUrl) {
         this.id = id;
         this.name = name;
